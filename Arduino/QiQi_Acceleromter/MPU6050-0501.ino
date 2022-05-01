25156bb6d27f5f4330af049c4c2d2267ae98b742
77malz <780400943@qq.com>
2022-05-01 13:09:04 +0100

    Create MPU6050-0501.ino

---
+++ Arduino/QiQi_Acceleromter/MPU6050-0501.ino
@@ -0,0 +1,60 @@
+/*
+   Arduino and MPU6050 Accelerometer and Gyroscope Sensor Tutorial
+*/
+#include <Wire.h>
+const int MPU = 0x68; // MPU6050 I2C address
+float AccX, AccY, AccZ;
+float GyroX, GyroY, GyroZ;
+float accAngleX, accAngleY, accAngleZ, gyroAngleX, gyroAngleY, gyroAngleZ;
+float roll, pitch, yaw;
+float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
+float elapsedTime, currentTime, previousTime;
+int c = 0;
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();                      // Initialize comunication
+  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
+  Wire.write(0x6B);                  // Talk to the register 6B
+  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
+  Wire.endTransmission(true);        //end the transmission
+  ///*
+  // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1C);                  //Talk to the ACCEL_CONFIG register (1C hex)
+  Wire.write(0x10);                  //Set the register bits as 00010000 (+/- 8g full scale range)
+  Wire.endTransmission(true);
+  // Configure Gyro Sensitivity - Full Scale Range (default +/- 250deg/s)
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1B);                   // Talk to the GYRO_CONFIG register (1B hex)
+  Wire.write(0x10);                   // Set the register bits as 00010000 (1000deg/s full scale)
+  Wire.endTransmission(true);
+  delay(20);
+}
+void loop() {
+  // === Read acceleromter data === //
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
+  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value加速度
+  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
+  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
+  // Calculating Roll and Pitch from the accelerometer data
+  accAngleX = (atan(AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI); // -AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
+  accAngleY = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI); // -AccErrorY ~(-1.58)
+  accAngleZ = (atan(AccZ / sqrt(pow(AccX, 2) + pow(AccY, 2))) * 180 / PI);
+  
+  // Print the values on the serial monitor
+  Serial.print(AccX);//Projection of gravity on x axis
+  Serial.print("/");
+  Serial.print(AccY);
+  Serial.print("/");
+  Serial.print(AccZ);
+  Serial.print("===");
+  Serial.print(accAngleX);//Angle between the x axis and gravity
+  Serial.print("/");
+  Serial.print(accAngleY);
+  Serial.print("/");
+  Serial.println(accAngleZ);
+}
